1f44b3f0eaf0906777ab42822bf5b4d1df275c56
Pawandeep <pawan9209@gmail.com>
2026-03-04 16:50:53 +0000

    Stablised and slightly tidier build of ARM style Mini-CPU simulator

--- main.py
+++ main.py
@@ -16,23 +16,14 @@ print('Memory:', cpu.memory)
 
 #       =======================================================================================
 # 
-# ++ 1 ++
-# FETCH - DECODE - EXECUTE LOOP needs to reflect reality of CPUs: 
-#     - Program of instruction set
-#     - Parsed and encoded instructions, creating machine code
-#     - Machine code stored in memory blocks
-#     - Fetch() retrieving instruction machine code from memory and feeding into register - R0 is program counter 
-#       and memory address of next instruction
-#     - Decode() 
-#     - Execute()
-# ++ 2 ++ 
+# ++ 1 ++ 
 # I need clean error messages, comments, a STRONG README, design_notes.pdf, architecture diagram.
 # 
 # README: Project Overview, How it Works, Instruction Set, Example Program, How to Run,
 # Design Decisions, Future Improvements
 #
-# ++ 3 ++
+# ++ 2 ++
 # Debug mode next up
 #
-# ++ 4 ++
+# ++ 3 ++
 # ADVANCED: Pipeline simulation, cache simulation, Interrupt system, step execution
